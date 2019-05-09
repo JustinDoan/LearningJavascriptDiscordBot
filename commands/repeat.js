300236c3b44e4564e284be77ccdc60c7a3ea60b0
Justin Doan <JDoan@rxnt.com>
2019-05-09 13:05:02 -0400

    added you said

--- commands/repeat.js
+++ commands/repeat.js
@@ -2,5 +2,5 @@ module.exports = function (message) {
     let messageToRepeat = message.split(" ")
     messageToRepeat.shift()
     let finalMessage = messageToRepeat.join(" ");
-    return "\"" + finalMessage + "\""
+    return "You said \"" + finalMessage + "\""
 }
